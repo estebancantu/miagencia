3e2caa556a7f2897765d54df87cdf473a83709ef
Esteban Cantu <estebancantu@gmail.com>
2016-07-28 17:56:21 -0300

    Database table and column names capitalized. Added properties: hasRegistration, hasDomainCertificate, taxDebt, trafficTicketsDebt.

--- src/main/java/com/miagencia/core/model/SaleItem.java
+++ src/main/java/com/miagencia/core/model/SaleItem.java
@@ -1,6 +1,7 @@
 package com.miagencia.core.model;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -9,20 +10,49 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="SaleItems")
+@Table(name="SALE_ITEMS")
 public class SaleItem extends PersistableEntity {
 	
-	// TODO falta hacer algo con el id para terminar el mapeo, revisar
+	
+	
+	
 	@OneToOne(cascade=CascadeType.ALL)
-	@JoinColumn(name="vehicleId", nullable=false)
+	@JoinColumn(name="VEHICLE_ID", nullable=false)
 	private Vehicle vehicle;
 	
+	@Column(name="SELLING_PRICE", nullable=false)
 	private int sellingPrice;
 	
 	@Enumerated(EnumType.STRING)
+	@Column(name="STATUS", nullable=false)
 	private VehicleStatus status;
 	
+	@Column(name="DAYS_IN_DEALERSHIP")
 	private int daysInDealership;
+	
+   /** 
+    * Título de propiedad */
+	@Column(name="HAS_REGISTRATION")
+	private Boolean hasRegistration;
+	
+   /** 
+    * Certificado de dominio*/
+	@Column(name="HAS_DOMAIN_CERTIFICATE")
+	private Boolean hasDomainCertificate;
+	
+	
+   /** 
+    * Deuda patentes */
+	@Column(name="TAX_DEBT")
+	private int taxDebt;
+	
+   /** 
+    * Multas */
+	@Column(name="TRAFFIC_TICKETS_DEBT")
+	private int trafficTicketsDebt;
+	
+	
+	
 	
 	
 	private SaleItem() {
@@ -36,6 +66,19 @@ public class SaleItem extends PersistableEntity {
 		this.sellingPrice = sellingPrice;
 		this.status = VehicleStatus.AVAILABLE;
 		this.daysInDealership = 0;
+	}
+	
+	public SaleItem(Vehicle vehicle, int sellingPrice, boolean hasRegistration, boolean hasDomainCertificate, 
+			int taxDebt, int trafficTicketsDebt){
+		
+		this.vehicle = vehicle;
+		this.sellingPrice = sellingPrice;
+		this.status = VehicleStatus.AVAILABLE;
+		this.daysInDealership = 0;
+		this.hasRegistration = hasRegistration;
+		this.hasDomainCertificate = hasDomainCertificate;
+		this.taxDebt = taxDebt;
+		this.trafficTicketsDebt = trafficTicketsDebt;
 	}
 
 
@@ -69,6 +112,42 @@ public class SaleItem extends PersistableEntity {
 
 	public void setVehicle(Vehicle vehicle) {
 		this.vehicle = vehicle;
+	}
+
+
+	public boolean isHasRegistration() {
+		return hasRegistration;
+	}
+
+
+	public void setHasRegistration(boolean hasRegistration) {
+		this.hasRegistration = hasRegistration;
+	}
+
+
+	public boolean isHasDomainCertificate() {
+		return hasDomainCertificate;
+	}
+
+
+	public void setHasDomainCertificate(boolean hasDomainCertificate) {
+		this.hasDomainCertificate = hasDomainCertificate;
+	}
+
+	public int getTaxDebt() {
+		return taxDebt;
+	}
+
+	public void setTaxDebt(int taxDebt) {
+		this.taxDebt = taxDebt;
+	}
+
+	public int getTrafficTicketsDebt() {
+		return trafficTicketsDebt;
+	}
+
+	public void setTrafficTicketsDebt(int trafficTicketsDebt) {
+		this.trafficTicketsDebt = trafficTicketsDebt;
 	}
 
 }
