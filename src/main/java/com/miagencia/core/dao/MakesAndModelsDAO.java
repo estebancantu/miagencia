68b25958138a3f213f604f4d24a030aa4045c574
Esteban Cantu <estebancantu@gmail.com>
2016-08-14 18:53:36 -0300

    New method getAllMakesAndModels(). getAllMakesForVehicleType(Long vehicleType) deprecated. getAllModelsForMake(Long make) removed.

--- src/main/java/com/miagencia/core/dao/MakesAndModelsDAO.java
+++ src/main/java/com/miagencia/core/dao/MakesAndModelsDAO.java
@@ -1,15 +1,19 @@
 package com.miagencia.core.dao;
 
+import java.util.List;
 import java.util.Map;
+
+import com.miagencia.rest.dto.MakeDTO;
 
 public interface MakesAndModelsDAO {
 	
+	@Deprecated
 	public Map<Long, String> getAllMakesForVehicleType(Long vehicleType);
-	
-	public Map<Long, String> getAllModelsForMake(Long make);
 	
 	public String getMake(Long makeId);
 	
 	public String getModel(Long modelId);
+	
+	public List<MakeDTO> getAllMakesAndModels(Long vehicleType);
 
 }
