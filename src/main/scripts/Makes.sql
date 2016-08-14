cc6e982458106078b8f844a4bcb314f1fd325ecd
Esteban Cantu <estebancantu@gmail.com>
2016-08-14 19:23:20 -0300

    Database scructure changed now to contain infoauto codes. Infoauto makes and models added.

--- src/main/scripts/Makes.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `Makes` (
-  `id` int(11) NOT NULL,
-  `vehicleType` int(11) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
--- src/main/scripts/Models.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `Models` (
-  `id` int(11) NOT NULL,
-  `make` int(11) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
