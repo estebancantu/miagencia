7be9f2f9b3255f9941544ab114a8d0418ffacecf
Esteban Cantu <estebancantu@gmail.com>
2016-08-14 18:54:25 -0300

    New method getAllMakesAndModels() implemented. getAllMakesForVehicleType(Long vehicleType) deprecated. getAllModelsForMake(Long make) made private.

--- src/main/java/com/miagencia/core/dao/impl/MakesAndModelsDAOImpl.java
+++ src/main/java/com/miagencia/core/dao/impl/MakesAndModelsDAOImpl.java
@@ -1,9 +1,13 @@
 package com.miagencia.core.dao.impl;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+
+
+
 
 
 
@@ -19,6 +23,8 @@ import org.springframework.stereotype.Repository;
 
 import com.miagencia.core.dao.MakesAndModelsDAO;
 import com.miagencia.core.model.Client;
+import com.miagencia.rest.dto.MakeDTO;
+import com.miagencia.rest.dto.ModelDTO;
 
 @Repository
 public class MakesAndModelsDAOImpl implements MakesAndModelsDAO {
@@ -27,10 +33,44 @@ public class MakesAndModelsDAOImpl implements MakesAndModelsDAO {
 	@Autowired
 	private SessionFactory sessionFactory;
 	
+
+	
+	
+
+	
+	@Override
+	public List<MakeDTO> getAllMakesAndModels(Long vehicleType) {
+		
+		Query query = sessionFactory.getCurrentSession().createSQLQuery("select ID, INFOAUTO_MAKE, NAME from MAKES m where m.VEHICLE_TYPE = :vehicleType").setParameter("vehicleType", vehicleType);;
+		List result = query.list();
+		
+		List<MakeDTO> makes = new ArrayList<MakeDTO>();
+		
+		Iterator it = result.iterator();
+		while(it.hasNext()) {
+			Object[] makeObject= (Object[]) it.next();
+			MakeDTO makeDto = new MakeDTO();
+			makeDto.setId(Long.valueOf((Integer)makeObject[0]));
+			makeDto.setInfoautoMake(Long.valueOf((Integer)makeObject[1]));
+			makeDto.setName((String)makeObject[2]);
+			
+			// ir a buscar los modelos de este make, chequear con servicio
+			List<ModelDTO> models = getAllModelsForMake(makeDto.getId());
+			makeDto.setModels(models);
+			
+			makes.add(makeDto);
+		}
+		
+		return makes;
+	}
+	
+	
+	
+	@Deprecated
 	@Override
 	public Map<Long, String> getAllMakesForVehicleType(Long vehicleType) {
 		
-		Query query = sessionFactory.getCurrentSession().createSQLQuery("select id, name from Makes m where m.vehicleType = :vehicleType").setParameter("vehicleType", vehicleType);;
+		Query query = sessionFactory.getCurrentSession().createSQLQuery("select id, name from MAKES m where m.VEHICLE_TYPE = :vehicleType").setParameter("vehicleType", vehicleType);;
 		List result = query.list();
 		
 		Map<Long, String> makes = new HashMap<Long, String>();
@@ -43,19 +83,25 @@ public class MakesAndModelsDAOImpl implements MakesAndModelsDAO {
 		
 		return makes;
 	}
-
-	@Override
-	public Map<Long, String> getAllModelsForMake(Long make) {
+	
+	
+	private List<ModelDTO> getAllModelsForMake(Long make) {
 		
-		Query query = sessionFactory.getCurrentSession().createSQLQuery("select id, name from Models m where m.make = :make").setParameter("make", make);;
+		Query query = sessionFactory.getCurrentSession().createSQLQuery("select ID, COD_INFOAUTO, NAME from MODELS m where m.MAKE_ID = :make").setParameter("make", make);;
 		List result = query.list();
 		
-		Map<Long, String> models = new HashMap<Long, String>();
+		List<ModelDTO> models = new ArrayList<ModelDTO>();
 		
 		Iterator it = result.iterator();
 		while(it.hasNext()) {
 			Object[] modelObject= (Object[]) it.next();
-			models.put(Long.valueOf((Integer)modelObject[0]), (String)modelObject[1]);
+			ModelDTO modelDto = new ModelDTO();
+			modelDto.setId(Long.valueOf((Integer)modelObject[0]));
+			modelDto.setCodInfoauto(Long.valueOf((Integer)modelObject[1]));
+			modelDto.setName((String)modelObject[2]);
+			
+			models.add(modelDto);
+		
 		}
 		
 		return models;
@@ -69,7 +115,7 @@ public class MakesAndModelsDAOImpl implements MakesAndModelsDAO {
 			throw new IllegalArgumentException("makeId argument cannot be null");
 
 		
-		Query query = sessionFactory.getCurrentSession().createSQLQuery("select name from Makes m where m.id = :makeId").setParameter("makeId", makeId);;
+		Query query = sessionFactory.getCurrentSession().createSQLQuery("select name from MAKES m where m.ID = :makeId").setParameter("makeId", makeId);;
 		return (String)query.uniqueResult();
 
 		
@@ -82,7 +128,7 @@ public class MakesAndModelsDAOImpl implements MakesAndModelsDAO {
 			throw new IllegalArgumentException("modelId argument cannot be null");
 
 		
-		Query query = sessionFactory.getCurrentSession().createSQLQuery("select name from Models m where m.id = :modelId").setParameter("modelId", modelId);;
+		Query query = sessionFactory.getCurrentSession().createSQLQuery("select name from MODELS m where m.ID = :modelId").setParameter("modelId", modelId);;
 		return (String)query.uniqueResult();
 
 		
