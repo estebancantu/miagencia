614e6e65e6791f1fb1259995c7d4c483dd6fe70c
Esteban Cantu <estebancantu@gmail.com>
2016-10-07 02:13:12 -0300

    Delete method added.

--- src/main/java/com/miagencia/rest/controller/VehicleController.java
+++ src/main/java/com/miagencia/rest/controller/VehicleController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.miagencia.core.service.VehicleService;
+import com.miagencia.rest.dto.ClientDTO;
 import com.miagencia.rest.dto.VehicleDetailsDTO;
 import com.miagencia.rest.dto.VehicleSummaryDTO;
 
@@ -80,6 +81,17 @@ public class VehicleController {
 		return new ResponseEntity<VehicleDetailsDTO>(vehicleDetailsDto, responseHeaders, HttpStatus.OK);		
 	}
 	
+	
+	
+	@RequestMapping(value = "/{vehicleId}", method = RequestMethod.DELETE)
+	public @ResponseBody ResponseEntity<Void> deleteVehicle(@PathVariable long vehicleId) {
+		  
+		System.out.println("Fetching & Deleting Vehicle with id " + vehicleId);
+
+      
+      vehicleService.delete(vehicleId);
+      return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+	}
 	
 	
 
