63f6fa1bd4c19edf5e9b86aa02b7c17b195704b1
Esteban Cantu <estebancantu@gmail.com>
2016-10-10 23:46:43 -0300

    Broadcast service now publishes deal price to be shared among controllers.

--- src/main/webapp/app/src/app/services/broadcastService.js
+++ src/main/webapp/app/src/app/services/broadcastService.js
@@ -7,6 +7,7 @@ broadcastService.factory('broadcastService', function($rootScope, $timeout) {
 	var shared = {};
 
 	shared.carId = ' ';
+	shared.initialProfit = ' ';
 
 	shared.broadcast = function( carId ) {
 
@@ -14,10 +15,23 @@ broadcastService.factory('broadcastService', function($rootScope, $timeout) {
 		this.notifyAll();
 	};
 
+	shared.postInitialProfit = function( initialProfit ) {
+
+		this.initialProfit = initialProfit;
+		this.notifyInitialProfit();
+	};
+
 	shared.notifyAll = function() {
 
 		$timeout(function(){
 			$rootScope.$broadcast('carIdEvent');
+		}, 1000);
+	};
+
+	shared.notifyInitialProfit = function() {
+
+		$timeout(function(){
+			$rootScope.$broadcast('initialProfitEvent');
 		}, 1000);
 	};
 
